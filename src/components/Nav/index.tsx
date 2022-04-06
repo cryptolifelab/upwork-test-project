709beeaf7825ac175bbccc11cd89766cf7ad3d37
VasiletsFour <vasilets.seven@gmail.com>
2022-04-06 16:20:20 +0300

    refactor some mistake

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 const Job: FC = () => {
   return (
     <header className="App-header">
-      <img className="App-header__logo" src="/logo.svg"></img>
+      <img className="App-header__logo" src="/logo.svg"/>
     </header>
   );
 };
