75a7747be7bd03d39e47e30e67ec6deb1742cf6d
VasiletsFour <vasilets.seven@gmail.com>
2022-04-06 16:29:32 +0300

    Change order by component add context

--- src/components/OrderBy/index.tsx
+++ src/components/OrderBy/index.tsx
@@ -1,33 +1,25 @@
-import React, { FC, useState } from "react";
-
-import { OrderTypes } from "../../types/order";
+import React, {ChangeEvent, FC} from "react";
+import {OrderTypes} from "../../types/order";
 import "./index.css";
+import OrderContext from "../../contexts/OrderContext";
 
-const Job: FC = () => {
-  const [selected, setSelected] = useState(OrderTypes.Random);
+const OrderBy: FC = () => {
+    const {orderby, toggleOrder} = React.useContext(OrderContext);
 
-  const onOrderChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
-    if (
-      e.target.value === OrderTypes.Random ||
-      e.target.value === OrderTypes.Priority
-    ) {
-      setSelected(e.target.value);
-    }
-  };
-  return (
-    <div className="App-sortby">
-      <span className="App-sortby__label">Sort by: </span>
-      <select
-        className="App-sortby__select"
-        id="filtersortby"
-        onChange={onOrderChange}
-        value={selected}
-      >
-        <option value={OrderTypes.Random}>Random</option>
-        <option value={OrderTypes.Priority}>By Priority</option>
-      </select>
-    </div>
-  );
+    return (
+        <div className="App-sortby">
+            <span className="App-sortby__label">Sort by: </span>
+            <select
+                className="App-sortby__select"
+                id="filtersortby"
+                onChange={(e:ChangeEvent<HTMLSelectElement>)=>toggleOrder(e.target.value)}
+                value={orderby}
+            >
+                <option value={OrderTypes.Random}>Random</option>
+                <option value={OrderTypes.Priority}>By Priority</option>
+            </select>
+        </div>
+    );
 };
 
-export default Job;
+export default OrderBy;
