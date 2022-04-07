41d282a1d023fd645c12ff5179695e3cff52c7a5
AhmadBilalVM <ahmed.bilal@visiomate.com>
2022-04-07 17:10:53 +0500

    test cases added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,6 +6,8 @@ test("renders joblist", async () => {
   render(<App />);
   setTimeout(async () => {
     const jobsElement = screen.getByTestId("app-jobs");
+    const previousButton = screen.getByRole("button");
+    expect(previousButton).toBeDisabled();
     expect(jobsElement).toBeInTheDocument();
   }, 5000);
 });
